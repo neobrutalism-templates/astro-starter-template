b2cb31800ea66d78ba5d36eec324223a782b89ff
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-08-08 02:57:54 +0200

    updated tw config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,37 +4,37 @@ export default {
   theme: {
     extend: {
       colors: {
-        main: "#88aaee",
-        mainAccent: "#4d80e6", // not needed for shadcn components
-        overlay: "rgba(0,0,0,0.8)",
-        // background color overlay for alert dialogs, modals, etc.
-
+        main: '#88aaee',
+        mainAccent: '#4d80e6', // not needed for shadcn components
+        overlay: 'rgba(0,0,0,0.8)', // background color overlay for alert dialogs, modals, etc.
+  
         // light mode
-        bg: "#dfe5f2",
-        text: "#000",
-        border: "#000",
-
+        bg: '#dfe5f2',
+        text: '#000',
+        border: '#000',
+  
         // dark mode
-        darkBg: "#272933",
-        darkText: "#eeefe9",
-        darkBorder: "#000",
+        darkBg: '#272933',
+        darkText: '#eeefe9',
+        darkBorder: '#000',
+        secondaryBlack: '#1b1b1b', // opposite of plain white, not used pitch black because borders and box-shadows are that color 
       },
       borderRadius: {
-        base: "5px",
+        base: '5px'
       },
       boxShadow: {
-        light: "4px 4px 0px 0px #000",
-        dark: "4px 4px 0px 0px #000",
+        light: '4px 4px 0px 0px #000',
+        dark: '4px 4px 0px 0px #000',
       },
       translate: {
-        boxShadowX: "4px",
-        boxShadowY: "4px",
-        reverseBoxShadowX: "-4px",
-        reverseBoxShadowY: "-4px",
+        boxShadowX: '4px',
+        boxShadowY: '4px',
+        reverseBoxShadowX: '-4px',
+        reverseBoxShadowY: '-4px',
       },
       fontWeight: {
-        base: "500",
-        heading: "700",
+        base: '500',
+        heading: '700',
       },
     },
   },
